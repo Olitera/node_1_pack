19f817e000bbf6a8f3574586f825a576f7808d4b
Volha Melayok <neiwra@gmail.com>
2024-11-17 16:28:29 +0100

    feat: add index.js with implemented function

---
+++ index.js
@@ -0,0 +1,36 @@
+function calculateRPN(expression) {
+  const stack = [];
+  const items = expression.split(' ');
+
+  items.forEach((item) => {
+    if (!isNaN(item)) {
+      stack.push(+item);
+    } else {
+      const second = stack.pop();
+      const first = stack.pop();
+
+      switch (item) {
+        case '+':
+          stack.push(first + second);
+          break;
+        case '-':
+          stack.push(first - second);
+          break;
+        case '*':
+          stack.push(first * second);
+          break;
+        case '/':
+          stack.push(first / second);
+          break;
+        default:
+          throw new Error(`Invalid operator: ${item}`);
+      }
+    }
+  });
+
+  if (stack.length !== 1) {
+    throw new Error('Invalid RPN expression');
+  }
+
+  return stack.pop();
+}
